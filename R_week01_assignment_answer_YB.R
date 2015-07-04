d01c6b53bc28666730cbad84505a1ac9876b41cb
yubailibra <yubailibra@yahoo.com>
2015-07-04 12:12:53 -0400

    add the example outputs from the code

--- R_week01_assignment_answer_YB.R
+++ R_week01_assignment_answer_YB.R
@@ -1,13 +1,14 @@
 #1. Write a loop that calculates 12-factorial.
 rs=1;
 for(i in as.numeric(2:12)){rs=rs*i;} #use as.numeric to handle bigger numbers than integer, just in case of overflow
-print(rs)
+>print(rs)
+479001600
 
 #2 Show how to create a numeric vector that contains the sequence from 20 to 50 by 5.
 seq(20,50,5)
 
 #3. Show how to take a trio of input numbers a, b, and c and implement the quadratic equation.
-# this function returns the roots for quadratic equation
+# this function returns the roots for the input quadratic equation
 getQRoot<-function(a,b,c){
   a=as.complex(a)
   b=as.complex(b)
@@ -26,4 +27,20 @@ getQRoot<-function(a,b,c){
   }
     return(union((-b+sqrt(b^2-4*a*c))/(2*a), (-b-sqrt(b^2-4*a*c))/(2*a)))
 }
+#some test case
+>getQRoot(1,0,1)
+[1] 0+1i 0-1i
+
+>getQRoot(1,3,1)
+[1] -0.381966+0i -2.618034+0i
+
+>getQRoot(0,0,1)
+[1] "this is not a quardratic equation"
+[1] "no root exists!"
+numeric(0)
+
+>getQRoot(0,0,0)
+[1] "this is not a quardratic equation"
+[1] "there are infinity roots to this equation"
+numeric(0)
 
